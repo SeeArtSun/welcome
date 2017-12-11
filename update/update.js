476c662d8f3bafa32e564cfc107e988fbf9c2ca8
SeeArtSun <seeart.sun@gmail.com>
2017-12-12 05:51:41 +0900

    $push commands 대응
      * commands 종류가 다양해짐에 따라 switch-case로 분기

--- update/update.js
+++ update/update.js
@@ -1,31 +1,38 @@
 const update = (state, commands) => {
   const nextState = {};
   const stateKeys = Object.keys(state);
+  const commandsKeys = Object.keys(commands);
 
-  stateKeys.forEach(key => {
-    const originValue = state[key];
-    const nextValue = commands[key] && commands[key].$set;
-      
-    if(!!commands.$set) {
-      return Object.assign(nextState, commands.$set);
-    }
+  switch (commandsKeys[0]) {
+    case "$push":
+      return state.concat(commands.$push);
+    default:
+      stateKeys.forEach(key => {
+        const originValue = state[key];
+        const nextValue = commands[key] && commands[key].$set;
+          
+        if(!!commands.$set) {
+          return Object.assign(nextState, commands.$set);
+        }
 
-    if (!!nextValue) {
-      nextState[key] = nextValue;
-      return;
-    }
+        if (!!nextValue) {
+          nextState[key] = nextValue;
+          return;
+        }
 
-    if (
-      typeof originValue === "object" &&
-      Object.keys(originValue).length > 0
-    ) {
-      nextState[key] = update(originValue, commands[key]);
-    } else {
-      nextState[key] = originValue;
-    }
-  });
+        if (
+          typeof originValue === "object" &&
+          Object.keys(originValue).length > 0
+        ) {
+          nextState[key] = update(originValue, commands[key]);
+        } else {
+          nextState[key] = originValue;
+        }
+      });
+    break;
+  }
 
-return nextState;
+  return nextState;
 };
 
 module.exports = update;
